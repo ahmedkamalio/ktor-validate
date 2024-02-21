24d2df65f52b8993c59da9bc6df2e83048b14eab
Ahmed Kamal <info@ahmedmkamal.com>
2024-02-21 18:02:23 +0200

    Add Maven publish support to build script

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -3,10 +3,12 @@
 plugins {
     alias(libs.plugins.jvm)
     `java-library`
+    `maven-publish`
 }
 
 group = "com.amedmoore"
 version = "0.0.1"
+description = "Adds validation annotations support for Ktor and Request Validation plugin."
 
 repositories {
     mavenCentral()
@@ -27,5 +29,24 @@ testing {
 java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = rootProject.name
+            url = uri("https://maven.pkg.github.com/amedmoore/${rootProject.name}")
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_PASSWORD")
+            }
+        }
+    }
+
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
     }
 }
